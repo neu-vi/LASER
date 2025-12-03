b9a1ca39587c137a24ad668fd14cbca957198cad
Jerry Ding <13386637338@qq.com>
2025-12-03 18:15:56 -0500

    fix cython setup

--- inference_engine/utils/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-from Cython.Build import cythonize
-import numpy as np
-
-setup(
-    ext_modules=cythonize("*.pyx", language_level=3),
-    include_dirs=[np.get_include()],
-    define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")]
-)
