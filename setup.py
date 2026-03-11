37a3144982b12bee664ed5b0849217ef11363103
Jerry Ding <13386637338@qq.com>
2026-03-11 17:36:31 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ import numpy as np
 setup(
     ext_modules=cythonize(
         "inference_engine/utils/*.pyx",
-        compiler_directives={'language_level': "3"}
+        compiler_directives={'language_level': "3"},
+        force=True
     ),
     include_dirs=[np.get_include()]
 )
